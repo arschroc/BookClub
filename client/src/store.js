f26dd78159e4bed228eebbd9db063c1321611b45
arschroc <arschroc@gmail.com>
2019-04-17 10:35:31 -0700

    Update store for non chrome users

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,29 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+var store;
+
+// Only chrome can handle the redux dev tool
+// redux compose cannot handle a null or undefined middleware
+if (
+  window.navigator.userAgent.includes("Chrome") &&
+  process.env.NODE_ENV !== "production"
+) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 export default store;
